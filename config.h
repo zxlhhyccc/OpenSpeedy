3b22caca2833a15bcdc3661a1b30cfb954cc6c87
Game1024 <pengbin.xyz@gmail.com>
2025-11-08 22:43:52 +0800

    fix version number and window title

--- config.h
+++ config.h
@@ -18,7 +18,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define OPENSPEEDY_VERSION "v1.7.6"
+#define OPENSPEEDY_VERSION "v1.7.7"
 
 #define BRIDGE32_EXE "bridge32.exe"
 #define BRIDGE64_EXE "bridge64.exe"
